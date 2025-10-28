dd424390b5dfd680b8c56923334049216ede47f4
F-los <xoghl1124@gmail.com>
2025-10-28 10:55:30 +0900

    fix: schema update

--- prisma/migrations/20251024084531_update_customer/migration.sql
+++ prisma/migrations/20251024084531_update_customer/migration.sql
@@ -1,7 +1,6 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[name]` on the table `customers` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[phone_number]` on the table `customers` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[email]` on the table `customers` will be added. If there are existing duplicate values, this will fail.
   - Made the column `email` on table `customers` required. This step will fail if there are existing NULL values in that column.
@@ -9,9 +8,6 @@
 */
 -- AlterTable
 ALTER TABLE "public"."customers" ALTER COLUMN "email" SET NOT NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX "customers_name_key" ON "public"."customers"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "customers_phone_number_key" ON "public"."customers"("phone_number");
